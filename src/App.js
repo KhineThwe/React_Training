8a462a25b8e0111502d7e8ba18e85b4fe22bfeaa
Khine <khinezar70122107@gmail.com>
2023-01-10 09:09:11 -1200

    change

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Componentone  from './component/Component1';
 import Componenttwo  from './component/Component2';
 
 class App extends Component {
+  state = {
+    name:"something"
+  }
+
+  doSomething=()=>{
+    this.setState({
+      name:"doing"
+    })
+  }
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <App1/>
-        <Componentone name={["khine","zar","thwe"]}/>
-        <Componenttwo/>
+        {/* <App1/> */}
+        {/* <Componentone name={["khine","zar","thwe"]}/>
+        <Componenttwo/> */}
+        <h1>{this.state.name}</h1>
+       <button onClick={this.doSomething}>Click Me</button>
       </div>
     )
   }
