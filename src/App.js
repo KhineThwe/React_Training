c84bca888f5663df2a933a8338f804719647da16
Khine <khinezar70122107@gmail.com>
2023-01-12 11:33:23 -1200

    use ref hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React,{useState} from 'react';
+import React,{useRef, useState} from 'react';
 import Componentone from './component/Component1'
 
 const App = () => {
-  const [name,setName] = useState("Kyaw Kyaw");
-
+  const [name,setName] = useState([
+    {name:"kyaw"},
+    {name:"mgmg"},
+    {name:"mama"}
+  ]);
+  
+  const nameInput = useRef();
   const ChangeName = (params)=>{
-    setName(params);
+    setName([...name,{name:nameInput.current.value}]);
+  }
+
+  const deleteName = (params)=>{
+     let newval = name.filter((val)=>val.name !== params)
+     setName(newval)
   }
   return (
     <div>
-      <h1>{name}</h1>
+      {name.map((val)=>{
+        return <h1 key={val.name} onClick={()=>deleteName(val.name)}>{val.name}</h1>
+      })}
+      <input type="text" ref={nameInput}/>
       {/* <button onClick={()=>setName('Mg Mg')}>Change Name</button> */}
-      <button onClick={()=>ChangeName("This is new Params")}>Change Name</button>
+      {/* <button onClick={()=>ChangeName()}>Change Name</button> */}
+      <button onClick={ChangeName}>Change Name</button>
     </div>
   )
 }
